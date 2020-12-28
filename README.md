9faf5234b62170f4270325b77a355f2c874924e0
Muhammad Muhajir <muhajirframe@gmail.com>
2020-12-28 13:48:10 +0700

    .

---
+++ README.md
@@ -0,0 +1,4 @@
+## Learn more
+
+- https://www.openvim.com
+- https://vim-adventures.com -> game, pretty good to train muscle memory
---
+++ comment.go
@@ -0,0 +1,10 @@
+package main
+
+// commenting a function there are many ways
+// - vaI cmd + /
+// - vaI gc
+// - gcaI
+func test() {
+	print("aa")
+	print("aa")
+}
